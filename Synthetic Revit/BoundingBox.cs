88da153c20779b9e9cb55569aa97b43446317f32
Unknown <github@amcgoey.net>
2019-01-03 21:24:01 -0500

    Add Clipping Plane Get and Set Methods

--- Synthetic Revit/BoundingBox.cs
+++ Synthetic Revit/BoundingBox.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using revitBB = Autodesk.Revit.DB.BoundingBoxXYZ;
+using revitBBxyz = Autodesk.Revit.DB.BoundingBoxXYZ;
+using revitBBuv = Autodesk.Revit.DB.BoundingBoxUV;
 using revitTransform = Autodesk.Revit.DB.Transform;
 using revitXYZ = Autodesk.Revit.DB.XYZ;
+using revitUV = Autodesk.Revit.DB.UV;
 
 namespace Synthetic.Revit
 {
@@ -15,21 +17,78 @@ namespace Synthetic.Revit
     /// </summary>
     public class BoundingBoxXYZ
     {
-        internal BoundingBoxXYZ () { }
+        internal BoundingBoxXYZ() { }
 
         /// <summary>
         /// Return the bounding box's transform
         /// </summary>
         /// <param name="BoundingBoxXYZ"></param>
         /// <returns></returns>
-        public static revitTransform GetTransform (revitBB BoundingBoxXYZ)
+        public static revitTransform GetTransform(revitBBxyz BoundingBoxXYZ)
         {
             return BoundingBoxXYZ.Transform;
         }
 
-        public static revitXYZ GetOrigin (revitBB BoundingBoxXYZ)
+        /// <summary>
+        /// Given a Revit BoundingBoxXYZ, returns its origin point.
+        /// </summary>
+        /// <param name="BoundingBoxXYZ"></param>
+        /// <returns name="Origin"></returns>
+        public static revitXYZ GetOrigin(revitBBxyz BoundingBoxXYZ)
         {
             return BoundingBoxXYZ.Transform.Origin;
+        }
+
+        public static revitXYZ GetMax(revitBBxyz BoundingBoxXYZ)
+        {
+            return BoundingBoxXYZ.Max;
+        }
+
+        public static revitXYZ GetMin(revitBBxyz BoundingBoxXYZ)
+        {
+            return BoundingBoxXYZ.Min;
+        }
+
+        public static revitBBxyz SetMax(revitBBxyz BoundingBoxXYZ, revitXYZ PointXYZ)
+        {
+            BoundingBoxXYZ.Max = PointXYZ;
+            return BoundingBoxXYZ;
+        }
+
+        public static revitBBxyz SetMin(revitBBxyz BoundingBoxXYZ, revitXYZ PointXYZ)
+        {
+            BoundingBoxXYZ.Min = PointXYZ;
+            return BoundingBoxXYZ;
+        }
+    }
+
+    /// <summary>
+    /// Wrapper for Revit BoundingBoxUV elements
+    /// </summary>
+    public class BoundingBoxUV
+    {
+        internal BoundingBoxUV () { }
+
+        public static revitUV GetMax(revitBBuv BoundingBoxUV)
+        {
+            return BoundingBoxUV.Max;
+        }
+
+        public static revitUV GetMin(revitBBuv BoundingBoxUV)
+        {
+            return BoundingBoxUV.Min;
+        }
+
+        public static revitBBuv SetMax(revitBBuv BoundingBoxUV, revitUV PointUV)
+        {
+            BoundingBoxUV.Max = PointUV;
+            return BoundingBoxUV;
+        }
+
+        public static revitBBuv SetMin(revitBBuv BoundingBoxUV, revitUV PointUV)
+        {
+            BoundingBoxUV.Min = PointUV;
+            return BoundingBoxUV;
         }
     }
 }
