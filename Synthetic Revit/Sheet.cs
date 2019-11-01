57a3fb51e69d81e298c51136698095cb4587461c
Arthur McGoey <synthetic@amcgoey.net>
2019-11-01 18:45:37 -0400

    Add Sheet.AddRevisions method

---
+++ Synthetic Revit/Sheet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using RevitDB = Autodesk.Revit.DB;
+using RevitDoc = Autodesk.Revit.DB.Document;
+using RevitSheet = Autodesk.Revit.DB.ViewSheet;
+using RevitRevision = Autodesk.Revit.DB.Revision;
+
+using RevitServices.Transactions;
+using Revit.Elements;
+using DynaElem = Revit.Elements.Element;
+using DynaRevision = Revit.Elements.Revision;
+using DynaSheet = Revit.Elements.Views.Sheet;
+
+namespace Synthetic.Revit
+{
+    /// <summary>
+    /// Sheet wrapper class
+    /// </summary>
+    public class Sheet
+    {
+        internal Sheet() { }
+
+        /// <summary>
+        /// Adds a Revision to a Sheet
+        /// </summary>
+        /// <param name="Sheet">Dynamo wrapped Sheet element</param>
+        /// <param name="Revision">Dynamo wrapped Revision element</param>
+        /// <returns name="Sheet">Returns the modified sheet.</returns>
+        public static DynaSheet AddRevision(DynaSheet Sheet, DynaRevision Revision)
+        {
+            RevitSheet revitSheet = (RevitSheet)Sheet.InternalElement;
+            RevitRevision revitRevision = (RevitRevision)Revision.InternalElement;
+
+            RevitDoc document = revitSheet.Document;
+
+            List<RevitDB.ElementId> revisions = (List<RevitDB.ElementId>)revitSheet.GetAdditionalRevisionIds();
+
+            if (!revisions.Contains(revitRevision.Id))
+            {
+                revisions.Add(revitRevision.Id);
+            }
+
+            if (document.IsModifiable)
+            {
+                TransactionManager.Instance.EnsureInTransaction(document);
+                revitSheet.SetAdditionalRevisionIds(revisions);
+                TransactionManager.Instance.TransactionTaskDone();
+            }
+            else
+            {
+                using (Autodesk.Revit.DB.Transaction trans = new Autodesk.Revit.DB.Transaction(document))
+                {
+                    trans.Start("Set Revision on Sheet");
+                    revitSheet.SetAdditionalRevisionIds(revisions);
+                    trans.Commit();
+                }
+            }
+
+            return Sheet;
+        }
+    }
+}
