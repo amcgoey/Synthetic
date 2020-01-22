8a92f390f3ccad15a7b6a753aada260a7c842d1b
Unknown <github@amcgoey.net>
2020-01-21 19:13:15 -0500

    Add Lines Class.  Add MergeLineStyles method

---
+++ Synthetic Revit/Lines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.DesignScript.Runtime;
+using Dynamo.Graph.Nodes;
+using RevitServices.Transactions;
+
+using Revit.Elements;
+
+using RevitDB = Autodesk.Revit.DB;
+using RevitElem = Autodesk.Revit.DB.Element;
+using RevitElemId = Autodesk.Revit.DB.ElementId;
+using RevitDoc = Autodesk.Revit.DB.Document;
+
+namespace Synthetic.Revit
+{
+    public class Lines
+    {
+        internal Lines () {}
+
+        public static IDictionary MergeLineStyles (RevitDB.Category FromLineStyle, RevitDB.Category ToLineStyle)
+        {
+
+        }
+    }
+}
