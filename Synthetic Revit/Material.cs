ffbc5db13b6690d2b275c6c456e9c9f1ad0685e5
Arthur <github@amcgoey.net>
2022-05-18 16:32:05 -0400

    Add GetMaterialBitmapPaths method to Materials

--- Synthetic Revit/Material.cs
+++ Synthetic Revit/Material.cs
@@ -10,6 +10,7 @@ using Dynamo.Graph.Nodes;
 using revitDB = Autodesk.Revit.DB;
 using revitDoc = Autodesk.Revit.DB.Document;
 using revitMaterial = Autodesk.Revit.DB.Material;
+using Autodesk.Revit.DB.Visual;
 
 using Revit.Elements;
 using dynElem = Revit.Elements.Element;
@@ -55,5 +56,78 @@ namespace Synthetic.Revit
         {
             return GetByNameDocument(Name, Synthetic.Revit.Document.Current());
         }
+
+        /// <summary>
+        /// Gets all the connected files with paths associated with a material.
+        /// </summary>
+        /// <param name="Material">A revit material</param>
+        /// <returns name="Paths">Full file names with paths</returns>
+        public static List<string> GetMaterialBitmapPaths (revitMaterial Material)
+        {
+            List<string> paths = new List<string>();
+
+            revitDB.ElementId appearanceAssetID = Material.AppearanceAssetId;
+            revitDB.AppearanceAssetElement assetElem = Material.Document.GetElement(appearanceAssetID) as revitDB.AppearanceAssetElement;
+
+            Asset renderingAsset = assetElem.GetRenderingAsset();
+
+            for (int idx=0; idx < renderingAsset.Size; idx++)
+            {
+                AssetProperty property = renderingAsset.Get(idx);
+                paths.AddRange(_ReadAssetPropertyPaths(property));
+            }
+
+            return paths;
+        }
+
+        /// <summary>
+        /// Checks if a AssetProperty has connected properties and if those connected properties have a bitmap path, it returns the path.
+        /// </summary>
+        /// <param name="assetProperty">A Revit AssetProperty element</param>
+        /// <returns name="paths">Bitmap paths</returns>
+        private static List<string> _ReadAssetPropertyPaths(AssetProperty assetProperty)
+        {
+            List<string> paths = new List<string>();
+
+            Asset connectedAsset = assetProperty.GetSingleConnectedAsset();
+            if (connectedAsset != null)
+            {
+                AssetPropertyString bitmapProperty = connectedAsset.FindByName(UnifiedBitmap.UnifiedbitmapBitmap) as AssetPropertyString;
+
+                if (bitmapProperty == null)
+                {
+                    bitmapProperty = connectedAsset.FindByName(BumpMap.BumpmapBitmap) as AssetPropertyString;
+                }
+                if (bitmapProperty != null)
+                {
+                    paths.Add(bitmapProperty.Value);
+                }                    
+            }
+
+            return paths;
+        }
+        //public static revitMaterial EditAppearancePaths (revitMaterial Material, List<string> Paths)
+        //{
+        //    revitDB.ElementId appearanceAssetID = Material.AppearanceAssetId;
+        //    revitDB.AppearanceAssetElement assetElem = Material.Document.GetElement(appearanceAssetID) as revitDB.AppearanceAssetElement;
+
+        //    using (revitDB.Transaction t = new revitDB.Transaction(Material.Document, "Change material bumpmap bitmap"))
+        //    {
+        //        t.Start();
+        //        using (AppearanceAssetEditScope editScope = new AppearanceAssetEditScope(assetElem.Document))
+        //        {
+        //            Asset editableAsset = editScope.Start(assetElem.Id);
+
+        //            //AssetProperty bumpMapProperty = editableAsset[Generic.GenericBumpMap];
+                    
+
+        //        }
+        //    }
+
+        //    return Material;
+        //}
+
+        
+        
     }
 }
